13cb7a4e4310c28f806956a1aa7425148e132994
shaweiguo <shaweiguo@ymail.com>
2020-10-06 22:14:25 +0800

    implemented default strategy and request-scoped strategies, extending guards.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,3 @@
-import { JwtStrategy } from './jwt.strategy';
 import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from './../users/users.module';
@@ -6,11 +5,14 @@ import { PassportModule } from "@nestjs/passport";
 import { LocalStrategy } from "./local.strategy";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtConstants } from "./constants";
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     forwardRef(() => UsersModule),
-    PassportModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: {
